2723eea4d45c531d97f4846026a5d060098a78e0
edgarbarrero <edgar.barrero@factorial.co>
2025-07-06 13:28:00 +0200

    wip

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar: React.FC = () => {
           {/* Navigation Items */}
           <div className="flex items-center space-x-8">
             <a 
-              href="/" 
+              href="#" 
               className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200"
             >
               Home
